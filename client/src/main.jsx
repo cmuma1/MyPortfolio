27b4546f63f59c1b5d1153ed6598d70f901b8b87
cmuma1 <cmuma1@my.centennialcollege.ca>
2025-12-04 22:04:10 -0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
+// client/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "../../server.js";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
